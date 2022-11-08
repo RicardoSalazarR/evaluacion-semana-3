431155fd0f3fcce39f6296f0d77b1852a130da06
Ricardo Salazar <axel.111yo@gmail.com>
2022-11-07 21:47:25 -0600

    e actualiza el color del status

--- src/components/Residents.jsx
+++ src/components/Residents.jsx
@@ -14,7 +14,13 @@ const Residents = ({ resident }) => {
         <li className='character-card'>
             <img src={character.image} alt="" />
             <b className='character-name'>{character.name}</b>
-            <p><b>status:</b> {character.status}</p>
+            <p className='status'>
+                <b>status:</b>
+                <div className='circle'
+                    style={character.status == 'Dead' ?
+                        { backgroundColor: 'red' } : character.status == 'Alive' ?
+                            { backgroundColor: 'green' } : { backgroundColor: 'gray' }}>
+                </div>{character.status}</p>
             <p><b>origin:</b> {character.origin?.name}</p>
             <p><b>Episodes where appear:</b> {character.episode?.length}</p>
         </li>
